7b26fd9ccceed85246bdabb4db936898f81a11f4
Kevin Silveira <k.ironhide9@yahoo.com>
2023-07-16 03:11:51 -0400

    fsUtils js file has been added under Helpers folder. Application now runs on local host with usable features.

---
+++ helpers/fsUtils.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const util = require('util');
+
+
+// fs.readFile
+
+const readFromFile = util.promisify(fs.readFile);
+
+
+// create new file
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+
+// Read new file object and Add
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
+
