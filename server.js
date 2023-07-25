9c8f307dd1424a745c0ec98d082e90bb82099daf
Kevin Silveira <k.ironhide9@yahoo.com>
2023-07-25 12:45:21 -0400

    deleted notes.js and removed get/post routes and app.API from server.js.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+
 //PORT ACTIVE
 const PORT = process.env.PORT || 3001;
 
@@ -10,20 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+
 app.use(express.static('public'));
 
 
-// GET route for Notes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-
-// GET route for Homepage
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 
 // INITIATE SERVER
-app.listen(PORT, () => { console.log(`App listening at http://localhost:${PORT} 🚀`);  // http://localhost:3001 in broswer for project testing
+app.listen(PORT, () => { console.log(`App listening at  http://localhost:${PORT} 🚀`);  // http://localhost:3001 in broswer for project testing
 });
