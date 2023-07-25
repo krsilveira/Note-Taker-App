cf8f61fbb36a3eddcc38c919996c688117522308
Kevin Silveira <k.ironhide9@yahoo.com>
2023-07-25 04:08:58 -0400

    notes.js file organized

--- routes/notes.js
+++ routes/notes.js
@@ -13,17 +13,15 @@ notes.get('/', (req, res) => { readFromFile('./db/db.json').then((data) => {
 });
 
 // specific note gets retrieved 
-notes.get('/:id', (req, res) => {
-  const noteId = req.params.id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
+notes.get('/:id', (req, res) => { const noteId = req.params.id;
+  readFromFile('./db/db.json') .then((data) => JSON.parse(data)) .then((json) => {
 
       const result = json.filter((note) => note.id === noteId);
       return result.length > 0
         ? res.json(result)
         : res.json('No note with that ID'); }); // error message when note does not exist
 });
+
 
 
 // notes.deleted function responds to a specific note the User selects for deletion when clicking the trashcan icon
@@ -44,17 +42,17 @@ notes.delete('/:id', (req, res) => {
 
 notes.post('/', (req, res) => { console.log(req.body);
 
-  const { title, text } = req.body;
-  if (req.body) {
-    const newNote = { title, text, id: uuidv4(),
+  const { title, text } = req.body; if (req.body) { const newNote = { title, text, id: uuidv4(),
      };
 
+     //response is recorded on db.json file
     readAndAppend(newNote, './db/db.json');
     res.json(`Note added successfully 🚀`); // response to successful note addidtion
   } else {
     res.error('Error in adding note'); // response Error message indicating in unsuccessful note addition 
   }
 });
+
 
 
 //module export
